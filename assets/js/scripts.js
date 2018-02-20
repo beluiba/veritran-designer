b1b108392392b52681780a72ebed23ca1cf8a124
Belen Ibanez Local <belenibanez@belen-imac.veritran.local>
2018-02-20 16:45:36 -0300

    improvements

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,7 +25,7 @@ jQuery(document).ready(function() {
     */
     $('.f1 fieldset:first').fadeIn('slow');
 
-    $('.f1 input[type="text"], .f1 input[type="password"], .f1 input[type="checkbox"], .f1 input[type="radio"], .f1 input[type="file"], .f1 textarea').on('focus', function() {
+    $('.f1 input[type="text"], .f1 input[type="password"], .f1 input[type="checkbox"], .f1 input[type="radio"], .f1 input[type="file"], select, .f1 textarea').on('focus', function() {
         $(this).removeClass('input-error');
     });
 
@@ -40,7 +40,7 @@ jQuery(document).ready(function() {
         var progress_line = $(this).parents('.f1').find('.f1-progress-line');
 
         // fields validation
-        parent_fieldset.find('input[type="text"], input[type="password"], input[type="radio"], input[type="checkbox"], input[type="file"], textarea').each(function() {
+        parent_fieldset.find('input[type="text"], input[type="password"], input[type="radio"], input[type="checkbox"], input[type="file"], select, textarea').each(function() {
             if ($(this).val() == "") {
                 $(this).addClass('input-error');
                 next_step = false;
@@ -96,7 +96,7 @@ jQuery(document).ready(function() {
     $('.f1').on('submit', function(e) {
 
         // fields validation
-        $(this).find('input[type="text"], input[type="password"], input[type="radio"], input[type="checkbox"], input[type="file"], textarea').each(function() {
+        $(this).find('input[type="text"], input[type="password"], input[type="radio"], input[type="checkbox"], input[type="file"], select, textarea').each(function() {
             if ($(this).val() == "") {
                 e.preventDefault();
                 $(this).addClass('input-error');
